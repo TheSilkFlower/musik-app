5c9990d8e67e1f9566d1f6eafe284bf61dcfff9c
TheSilkFlower <smirnova100991@gmail.com>
2023-12-26 14:45:19 +0300

    Удалила import из style.js

--- src/lib/style.js
+++ src/lib/style.js
@@ -2,12 +2,5 @@
 
 // Place other JavaScript modules in the 'lib' folder
 import 'assets/styles/index.less'
-import Swiper from 'swiper'
-
-console.log('Style applied')
-const swiper = new Swiper('.swiper', {
-speed: 400,
-spaceBetween: 100,
-})
 
 export default 'style module'
