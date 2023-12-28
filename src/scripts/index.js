255185a7deeb0b7edf0a457e55388ba84a0fba02
TheSilkFlower <smirnova100991@gmail.com>
2023-12-28 22:52:10 +0300

    Инициализировала swiper в index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,18 @@
 
 import '../lib/style'
 console.log('Hello World')
+
+new window.Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  pagination: {
+    el: '.swiper-pagination'
+  }
+})
