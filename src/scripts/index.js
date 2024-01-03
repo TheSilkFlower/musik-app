c4961e7139ce4951aaa75b5b6b0151c542face5a
TheSilkFlower <smirnova100991@gmail.com>
2024-01-03 19:20:02 +0300

    Временно удалила swiper из блока genres

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,31 +4,3 @@ import '../lib/style'
 console.log('Hello World')
 
     // eslint-disable-next-line no-unused-vars
-const swiper = new window.Swiper('.swiper', {
-  init: true,
-  breakpoints: {
-    1600: {
-      slidesPerView: 5,
-      spaceBetween: 30
-    },
-    1200: {
-      slidesPerView: 1,
-      spaceBetween: 30
-    }
-  },
-  spaceBetween: 30,
-  loop: true,
-  observer: true,
-  observeParents: true,
-  observeSlideChildren: true,
-  effect: 'fade',
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-
-  pagination: {
-    el: '.swiper-pagination'
-  }
-})
