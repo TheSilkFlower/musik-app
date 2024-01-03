19bc5a4e12aa6badb0e1fd3a42d690acca1b3c07
TheSilkFlower <smirnova100991@gmail.com>
2024-01-03 18:28:13 +0300

    Добавила breakpoints для swiper

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,27 +3,32 @@
 import '../lib/style'
 console.log('Hello World')
 
-const width = window.innerWidth
-document.addEventListener('DOMContentLoaded', () => {
-  if (width < 1200) {
     // eslint-disable-next-line no-unused-vars
-    const swiper = new window.Swiper('.swiper', {
-      init: true,
+const swiper = new window.Swiper('.swiper', {
+  init: true,
+  breakpoints: {
+    1600: {
+      slidesPerView: 5,
+      spaceBetween: 30
+    },
+    1200: {
       slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      observer: true,
-      observeParents: true,
-      observeSlideChildren: true,
+      spaceBetween: 30
+    }
+  },
+  spaceBetween: 30,
+  loop: true,
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  effect: 'fade',
 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
 
-      pagination: {
-        el: '.swiper-pagination'
-      }
-    })
+  pagination: {
+    el: '.swiper-pagination'
   }
 })
