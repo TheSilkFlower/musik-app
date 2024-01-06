0795697ec726af28bbe52dce6d9536a89c0d5cd7
TheSilkFlower <smirnova100991@gmail.com>
2024-01-06 16:57:58 +0300

    Подключила обработчик клика с функцией makeActive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,4 +3,27 @@
 import '../lib/style'
 console.log('Hello World')
 
-    // eslint-disable-next-line no-unused-vars
+function makeActive () {
+  let allNumerations = document.querySelectorAll('.events__numeration')
+  for (let num of allNumerations) {
+    num.style.opacity = '0.5'
+  }
+  let allNumsOfMonth = document.querySelectorAll('.events__num-of-month')
+  for (let num of allNumsOfMonth) {
+    num.style.opacity = '0.3'
+  }
+  let allLines = document.querySelectorAll('.events__line')
+  for (let num of allLines) {
+    num.style.opacity = '0.1'
+  }
+  let numeration = this.querySelector('.events__numeration')
+  let numOfMonth = this.querySelector('.events__num-of-month')
+  let line = this.querySelector('.events__line')
+  numeration.style.opacity = '1'
+  numOfMonth.style.opacity = '1'
+  line.style.opacity = '1'
+}
+
+document.querySelectorAll('.events__date').forEach((elem) => {
+  elem.addEventListener('click', makeActive)
+})
