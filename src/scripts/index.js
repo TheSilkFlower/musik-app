66c06c05f9db973d78870b2722d1ceb1e757fa19
TheSilkFlower <smirnova100991@gmail.com>
2024-05-08 21:43:23 +0300

    Изменила путь к картинке

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -75,7 +75,7 @@ function createDinamicElements () {
       link.href = el[4]
       const linkImg = document.createElement('img')
       linkImg.classList.add('events-table__network-img')
-      linkImg.src = '/img/arr2.png'
+      linkImg.src = './img/aiga.svg'
       link.append(linkImg)
 
       // вносим данные из массива в таблицу
