318dc394455e38a61f00e9eb33843436ebed4489
TheSilkFlower <smirnova100991@gmail.com>
2024-06-10 00:28:30 +0300

    Add build

---
+++ dist/static/js/anchors.867445864e0339731c49.js
@@ -0,0 +1,1 @@
+webpackJsonp([6,7],{57:function(e,t,o){"use strict";document.querySelector(".button_red").addEventListener("click",function(){document.querySelector("#target-anchor").scrollIntoView({behavior:"smooth"})})}},[57]);
---
+++ dist/static/js/page2.88f1b1bf623a75148f21.js
@@ -0,0 +1,1 @@
+webpackJsonp([4,7],{56:function(e,n,t){"use strict";var c=t(59);t.n(c)},59:function(e,n){},61:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});t(56)}},[61]);
